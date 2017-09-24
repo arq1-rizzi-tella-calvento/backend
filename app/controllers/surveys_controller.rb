49fd58f7df93154ceccc835f0a16e28693ccf0ac
Julian Calvento <juliancalvento@gmail.com>
2017-09-24 13:19:01 -0300

    New survey route. Versión específica de ruby

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,4 +2,14 @@ class SurveysController < ApplicationController
   def create
     render json: {}, status: :ok
   end
+
+  def new
+    survey = {
+      1 => ['c1', 'c2', 'no-cursar', 'ya-curso'],
+      2 => ['c1', 'c2', 'no-cursar', 'ya-curso'],
+      3 => ['c1', 'no-cursar', 'ya-curso'],
+    }
+
+    render json: survey, status: :ok
+  end
 end
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -8,4 +8,20 @@ describe SurveysController do
       expect(response.status).to eq 200
     end
   end
+
+  context 'GET #new' do
+    let(:expected_response) do
+      {
+        '1' => ['c1', 'c2', 'no-cursar', 'ya-curso'],
+        '2' => ['c1', 'c2', 'no-cursar', 'ya-curso'],
+        '3' => ['c1', 'no-cursar', 'ya-curso'],
+      }
+    end
+
+    it 'returns the questions' do
+      get :new
+
+      expect(JSON.parse(response.body)).to eq expected_response
+    end
+  end
 end
