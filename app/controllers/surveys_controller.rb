c7bf6ec9ad1682ddaaf97d2497aba670a2310860
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-10-04 19:35:50 -0300

    Cambios en el payload de la encuesta

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,14 +1,31 @@
 class SurveysController < ApplicationController
   def create
-    render json: {message: 'Survey successfully submitted'}, status: :ok
+    rand
+    render json: { message: 'Survey successfully submitted' }, status: :ok
   end
 
   def new
-    survey = {
-      1 => ['c1', 'c2', 'no-cursar', 'ya-curso'],
-      2 => ['c1', 'c2', 'no-cursar', 'ya-curso'],
-      3 => ['c1', 'no-cursar', 'ya-curso'],
-    }
+    survey = [
+      {
+        name: 'Estructura de datos',
+        options: ['C1 - 21-22hs Miercoles', 'C2 - 21-22hs Jueves']
+      },
+      {
+        name: 'Matematica II',
+        options: ['C1 - 8-12hs Viernes', 'C2 - 8-12hs Lunes']
+      },
+      {
+        name: 'Ingles I',
+        options: ['C1 - 14-16hs Jueves', 'C2 - 14-16hs Martes']
+      },
+      {
+        name: 'Programación concurrente',
+        options: ['C1 - 18-22hs Martes', 'C2 - 18-22hs Viernes']
+      }
+    ]
+
+    common_options = ['Ya curse', 'Todavia no voy a cursar']
+    survey.each { |question| question[:options].concat(common_options) }
 
     render json: survey, status: :ok
   end
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -11,17 +11,36 @@ describe SurveysController do
 
   context 'GET #new' do
     let(:expected_response) do
-      {
-        '1' => ['c1', 'c2', 'no-cursar', 'ya-curso'],
-        '2' => ['c1', 'c2', 'no-cursar', 'ya-curso'],
-        '3' => ['c1', 'no-cursar', 'ya-curso'],
-      }
+      [
+        {
+          name: 'Estructura de datos',
+          options: ['C1 - 21-22hs Miercoles', 'C2 - 21-22hs Jueves', 'Ya curse', 'Todavia no voy a cursar']
+        },
+        {
+          name: 'Matematica II',
+          options: ['C1 - 8-12hs Viernes', 'C2 - 8-12hs Lunes', 'Ya curse', 'Todavia no voy a cursar']
+        },
+        {
+          name: 'Ingles I',
+          options: ['C1 - 14-16hs Jueves', 'C2 - 14-16hs Martes', 'Ya curse', 'Todavia no voy a cursar']
+        },
+        {
+          name: 'Programación concurrente',
+          options: ['C1 - 18-22hs Martes', 'C2 - 18-22hs Viernes', 'Ya curse', 'Todavia no voy a cursar']
+        }
+      ]
     end
 
     it 'returns the questions' do
       get :new
 
-      expect(JSON.parse(response.body)).to eq expected_response
+      survey = JSON.parse(response.body, symbolize_names: true)
+
+      expect(survey).to eq expected_response
+    end
+
+    def commission(name, schedule, day)
+      { name: name, class_schedule: schedule, day: day }
     end
   end
 end
