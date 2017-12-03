e3f0ee63dd0935ded7028020583be029e869326d
patriciotella <patriciotella@gmail.com>
2017-12-03 20:11:22 -0300

    Mejoras:
    - Se extraen los metodos del controller a un helper.
    - Se corrigen nombres de variables
    - Se genera un json con las materias y el futuro link para edicion

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,9 +1,11 @@
 class SurveysController < ApplicationController
+  include SurveysHelper
+
   def create
-    success_message = ''
+    success_message = []
     params[:subjects].each do |subject|
       current_subject = Subject.find_by(name: subject[:name])
-      student_answer = subject[:condition]
+      student_answer = subject[:selectedChair]
       if student_answer == 'approve'
       #   tenemos que asignarla a las materias aprobadas no como una respuesta
       else
@@ -11,7 +13,7 @@ class SurveysController < ApplicationController
       end
     end
 
-    render json: { message: generate_success_message(success_message) }, status: :ok
+    render json: generate_success_message(success_message) , status: :ok
   end
 
   def new
@@ -27,38 +29,5 @@ class SurveysController < ApplicationController
 
   def student_id
     params.permit(:student_id)[:student_id]
-  end
-
-  def build_survey(survey_subjects)
-    survey_subjects.map { |subject| { name: subject.name, chairs: subject.chairs.map(&:time) } }
-  end
-
-  def submit_answer(current_subject, student_answer, subject, success_message)
-    Answer.new.tap do |answer|
-      answer.student_id = params[:userId]
-      if !student_answer || student_answer == 'cant' || student_answer == ''
-        reply_option = ReplyOption.new.tap do |replyOption|
-          replyOption.value = 'No voy a cursar'
-          replyOption.save!
-        end
-        answer.reply_option = reply_option
-      else
-        chair = current_subject.subject_in_quarter.chairs.detect { |chair| chair.time == subject[:condition] }
-        success_message = success_message + ' , ' + subject[:name] + ' comision: ' + chair.time
-        answer.chair_id = student_answer
-        answer.chair = chair
-      end
-      answer.save!
-    end
-    success_message
-  end
-
-  def generate_success_message(success_message)
-    if success_message.empty?
-      success_message = 'Survey successfully submitted'
-    else
-      success_message = 'Survey details : ' + success_message
-    end
-    success_message
   end
 end
