6b9fc307e6a7de053da9128b90653406ea67823e
Julian Calvento <juliancalvento@gmail.com>
2017-12-10 18:39:32 -0300

    Cambio la forma de agrupar las materias para la vista del director

--- spec/controllers/summary_controller_spec.rb
+++ spec/controllers/summary_controller_spec.rb
@@ -8,21 +8,26 @@ describe SummaryController do
     let(:intro) { create(:subject_in_quarter, subject: create(:subject, name: 'Intro'), survey: survey) }
     let!(:intro_chair_2) { create :chair, number: 2, subject_in_quarter: intro }
     let!(:intro_chair_1) { create :chair, number: 1, subject_in_quarter: intro }
-    let(:chairs) { [chair_1, intro_chair_1, intro_chair_2] }
-
-    it 'returns the subject name, chair, number of students and fullness percentage' do
-      expected_response = chairs.map do |chair|
+    let(:subjects) { [math_subject, intro] }
+    let(:expected_subjects_summary) do
+      subjects.map do |subject|
         {
-          name: chair.subject.name,
-          chair: chair.number,
-          number_of_students: chair.number_of_students,
-          fullness_percentage: chair.fullness_percentage
+          name: subject.name,
+          chairs: subject.chairs.reverse.map do |chair|
+            {
+              chair: chair.number,
+              number_of_students: chair.number_of_students,
+              fullness_percentage: chair.fullness_percentage
+            }
+          end
         }
       end
+    end
 
+    it 'returns the subject name, chair, number of students and fullness percentage' do
       get :index
 
-      expect(response_body[:subjects]).to eq expected_response
+      expect(response_body[:subjects]).to eq expected_subjects_summary
     end
 
     it 'returns the percentage of students that answered the survey' do
