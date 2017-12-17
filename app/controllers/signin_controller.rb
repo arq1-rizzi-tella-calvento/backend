29f2c1b3c598358adec25835dee1d10ccfc23cc9
Julian Calvento <juliancalvento@gmail.com>
2017-12-17 17:41:51 -0300

    Devuelve el dni y nombre para permitir redirigir automaticamente

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -2,7 +2,7 @@ class SigninController < ApplicationController
   def show
     student = academic_record.find_student_with(token: params[:id])
 
-    render json: { token: student.token }, status: :ok
+    render json: { token: student.token, name: student.name, identity_document: student.identity_document }, status: :ok
   rescue ActiveRecord::RecordNotFound
     head :not_found
   end
